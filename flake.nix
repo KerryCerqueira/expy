ac0dc6f2f2d27cb47be71320d6f5a7a0ce1e2209
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-07-04 16:10:59 -0400

    Rewrite flake.nix and pyproject.toml
    Expose package output in flake. Add toml dependency to flake. Rewrite
    dev shell to include live editable package. Remove inference
    dependencies.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
 	description = "Reproducible AI experiments with python";
-
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils .url = "github:numtide/flake-utils";
@@ -10,63 +9,54 @@
 		{ nixpkgs, flake-utils, ... }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
-				shellHook = # bash
-					''
-				python -m ipykernel install --user --name nix
-				'';
-			in { devShells = {
-					cpuInference = let
-						pkgs = import nixpkgs {
-							system = system;
-							config.allowUnfree = true;
-						};
-					in pkgs.mkShell {
-							buildInputs = with pkgs; [
-								(python3.withPackages ( ps: with ps; [
-									gitpython
-									torch
-									transformers
-									llama-cpp-python
-									ipython
-									ipykernel
-									langchain
-									langchain-community
-									langchain-huggingface
-									langgraph
-									nbclient
-									pydantic
-								]))
-								jupyter
-							];
-							inherit shellHook;
-						};
-					cudaInference = let
-						pkgs = import nixpkgs {
-							system = system;
-							config.allowUnfree = true;
-							config.cudaSupport = true;
-						};
-					in pkgs.mkShell {
-							buildInputs = with pkgs; [
-								(python3.withPackages ( ps: with ps; [
-									gitpython
-									torch
-									transformers
-									llama-cpp-python
-									ipython
-									ipykernel
-									langchain
-									langchain-community
-									langchain-huggingface
-									langgraph
-									nbclient
-									pydantic
-								]))
-								jupyter
-							];
-							LD_PRELOAD = "/usr/lib/x86_64-linux-gnu/libcuda.so.1";
-							inherit shellHook;
-						};
+				pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+				pkgs = import nixpkgs {
+					system = system;
+				};
+				expy = pkgs.python3Packages.buildPythonApplication {
+					pname = "expy";
+					inherit (pyproject.project) version;
+					format = "pyproject";
+					src = ./.;
+					build-system = with pkgs.python3Packages; [
+						setuptools
+					];
+					propagatedBuildInputs = with pkgs.python3Packages; [
+						gitpython
+						langgraph
+						nbclient
+						pydantic
+						typer
+					];
+				};
+				expy-dev = pkgs.python3.pkgs.mkPythonEditablePackage {
+					pname = "expy";
+					inherit (pyproject.project) scripts version;
+					root = "$PWD/src";
+				};
+			in {
+				packages = {
+					inherit expy;
+				};
+				devShells = {
+					default = pkgs.mkShell {
+						inputsFrom = [ expy ];
+						buildInputs = with pkgs; [
+							expy-dev
+							(python3.withPackages ( ps: with ps; [
+								ipython
+								ipykernel
+								langchain
+								langchain-community
+								langchain-huggingface
+							]))
+							jupyter
+						];
+						shellHook = # bash
+							''
+							python -m ipykernel install --user --name nix
+							'';
+					};
 				};
 			}
 		);
